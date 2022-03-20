b553772fda785789f33300137c836e173dff0432
TY <1441212902@qq.com>
2022-03-20 21:27:19 +0800

    Move config vars into config.js

---
+++ README.md
@@ -0,0 +1,32 @@
+# autosign
+
+此程序可以帮助你每日健康打卡。
+主要文件：
+auto.js: 主程序
+config.js: 设置文件
+
+# 在你的计算机上测试
+
+```bash
+sudo apt install git node
+git clone https://github.com/Shigure19/autosign.git
+cd autosign
+npm install
+node auto.js
+```
+
+# 部署到Docker
+
+待完成。
+
+# 部署到腾讯云函数
+
+待完成。
+
+# 部署到github action
+
+待完成。
+
+# TODO
+- [ ] 教程
+- [ ] 自动截图
--- auto.js
+++ auto.js
@@ -1,113 +1,107 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
-const winston = require('winston');
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console(),
-    ]
-});
-const request = require('request');
-
-const testMode = true;
-
-const users = [
-    {
-        "userId": "YourUserId",
-        "password": "YourPassword",
-        "needScreenshot": true,
-        "serverChanSendKey": 'YourSendKey'
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const logger = {
+    info: (msg) => {
+        var date = new Date();
+        var time = date.getTime();
+        console.log(time+msg);
     }
-]
-
-const headlessConfig = {
-    //关闭无头模式
-    headless: false,
-    //全屏打开浏览器
-    args: ['--start-maximized'],
-    //设置浏览器页面尺寸
-    defaultViewport: { width: 1800, height: 1000 }
 }
+const request = require("request");
+const users = require("./config.js").config.users;
+const browserConfig = require("./config.js").config.browser;
+const testMode = require("./config.js").config.public.testMode;
 const sendToWeChat = async (user2) => {
-    logger.info("正发送至微信");
-    if (user2.status = "success") {
-        let msg = "https://sctapi.ftqq.com/" + user2.serverChanSendKey + ".send?title=学号" + user2.userId + "健康打卡成功"
-        if (user2.screenshot != null) {
-            console.log("发送截图");
-            msg = msg + "&desp=" + user2.screenshotUri;
-        }
-        request(encodeURI(msg))
-    } else {
-        request("https://sctapi.ftqq.com/" + user2.sendKey + ".send?title=学号" + user2.userId + "健康打卡失败")
+  logger.info("正发送至微信");
+  let msg =
+    "https://sctapi.ftqq.com/" +
+    user2.serverChanSendKey +
+    ".send?title=学号" +
+    user2.userId;
+  if ((user2.status = "success")) {
+    msg = msg + "健康打卡成功";
+    if (user2.screenshot != null) {
+      console.log("发送截图");
+      msg = msg + "&desp=" + user2.screenshotUri;
     }
-}
+  } else {
+    msg = msg + "健康打卡失败";
+  }
+  request(encodeURI(msg));
+};
 (async () => {
-    for (const user of users) {
-        try {
-            logger.info("学号：" + user.userId + "开始打卡。");
-            const browser = await puppeteer.launch(headlessConfig);
-            const page = await browser.newPage();
-            await page.emulate(puppeteer.devices['iPhone X']);
-            await page.goto("https://workflow.ecust.edu.cn/default/work/uust/zxxsmryb/mrybtb.jsp", { waitUntil: 'networkidle0' });
-            logger.info('进入成功');
-            const usernameInput = await page.$("#mobileUsername");
-            await usernameInput.type(user.userId);//输入学号
-            logger.info("学号输入成功");
-            const passwordInput = await page.$("#mobilePassword");
-            await passwordInput.type(user.password);//输入密码
-            logger.info("密码输入成功");
-            await Promise.all([
-                page.waitForNavigation({
-                    waitUntil: 'networkidle0'
-                }),
-                logger.info("登录成功"),
-                page.click("#load")//点击登录
-            ])
-
-            const signed = await page.evaluate(async () => {
-                return flag
-            })
-            if (signed) {
-                if (testMode) {
-                    await page.evaluate(async () => {
-                        $("#layui-layer100001").remove();
-                        $("#layui-layer-shade100001").remove();
-                        flag = false;
-                    })
-                } else {
-                    logger.info("已打卡，下一个用户")
-                    await page.close();
-                    continue;
-                }
-            }
-            await page.click("#sui-select-swjkzk19");//健康
-            logger.info("已选择健康")
-            await page.click("#sui-select-xcm4");//行程码绿码
-            logger.info("已选择行程码绿码")
-            await page.click("#sui-select-twsfzc8");//体温正常
-            logger.info("已选择体温正常")
-            await page.click("#sui-select-jkmsflm12");//健康码绿码
-            logger.info("已选择健康码绿码")
-            await page.click("#sui-select-sfycxxwc33");//没从学校外出
-            logger.info("已选择没从学校外出")
-            await page.evaluate(async () => {
-                saveOrUpdate();
-            })
-            await page.click(".layui-layer-btn0");
-            logger.info("学号：" + user.userId + "已打卡")
-            if (user.needScreenshot) {
-                const tmpDir = require('os').tmpdir();
-                const date = new Date();
-                await page.screenshot({ path: path.join(tmpDir, date.getDate() + user.userId + '.png') });
-                //uploadtoImg
-                user["screenshotUri"] = "123456"
-            }
-            if (user.serverChanSendKey != null) {
-                sendToWeChat(user);
-            }
-            await browser.close();
-        } catch (e) {
-            console.log(e);
+  for (const user of users) {
+    try {
+      logger.info("学号：" + user.userId + "开始打卡。");
+      const browser = await puppeteer.launch(browserConfig);
+      const page = await browser.newPage();
+      await page.emulate(puppeteer.devices["iPhone X"]);
+      await page.goto(
+        "https://workflow.ecust.edu.cn/default/work/uust/zxxsmryb/mrybtb.jsp",
+        { waitUntil: "networkidle0" }
+      );
+      logger.info("进入成功");
+      const usernameInput = await page.$("#mobileUsername");
+      await usernameInput.type(user.userId); //输入学号
+      logger.info("学号输入成功");
+      const passwordInput = await page.$("#mobilePassword");
+      await passwordInput.type(user.password); //输入密码
+      logger.info("密码输入成功");
+      await Promise.all([
+        page.waitForNavigation({
+          waitUntil: "networkidle0",
+        }),
+        logger.info("登录成功"),
+        page.click("#load"), //点击登录
+      ]);
+      const signed = await page.evaluate(async () => {
+        return flag;
+      });
+      if (signed) {
+        if (testMode) {
+          await page.evaluate(async () => {
+            $("#layui-layer100001").remove();
+            $("#layui-layer-shade100001").remove();
+            flag = false;
+          });
+        } else {
+          logger.info("已打卡，下一个用户");
+          await page.close();
+          continue;
         }
+      }
+      await page.click("#sui-select-swjkzk19"); //健康
+      logger.info("已选择健康");
+      await page.click("#sui-select-xcm4"); //行程码绿码
+      logger.info("已选择行程码绿码");
+      await page.click("#sui-select-twsfzc8"); //体温正常
+      logger.info("已选择体温正常");
+      await page.click("#sui-select-jkmsflm12"); //健康码绿码
+      logger.info("已选择健康码绿码");
+      await page.click("#sui-select-sfycxxwc33"); //没从学校外出
+      logger.info("已选择没从学校外出");
+      await page.evaluate(async () => {
+        saveOrUpdate();
+      });
+      await page.click(".layui-layer-btn0");
+      logger.info("学号：" + user.userId + "已打卡");
+      if (user.needScreenshot) {
+        const tmpDir = require("os").tmpdir();
+        const date = new Date();
+        await page.screenshot({
+          path: path.join(tmpDir, date.getDate() + user.userId + ".png"),
+        });
+        //uploadtoImg
+        user["screenshotUri"] = "123456";
+      }
+      if (user.serverChanSendKey != null) {
+        sendToWeChat(user);
+      }
+      await browser.close();
+    } catch (e) {
+      console.log(e);
+      continue;
     }
+  }
 })();
