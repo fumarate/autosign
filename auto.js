f480702aa3545e786d1c9cc5e550db6ccc6a9cdb
TY <1441212902@qq.com>
2022-03-21 21:56:03 +0800

    Better log

--- auto.js
+++ auto.js
@@ -1,34 +1,48 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
-const logger = {
-  info: (msg) => {
-    var date = new Date();
-    var time = date.getTime();
-    console.log(time + msg);
-  },
-};
-const params = process.argv.slice(2);
+const moment = require("moment");
+const colors = require("colors");
+class Logger {
+  constructor() {
+    this.userId = null;
+  }
+  info(msg) {
+    const timeStr = moment().format("YYYY-MM-DD HH:mm:ss");
+    const source = this.userId != null ? this.userId : "info";
+    console.log(timeStr.green + " " + source + " " + msg);
+  }
+}
+let logger = new Logger();
 const request = require("request");
 const users = require("./config.js").config.users;
 const browserConfig = require("./config.js").config.browser;
 const publicConfig = require("./config.js").config.public;
-const sendToWeChat = async (user2) => {
-  logger.info("正发送至微信");
-  let msg =
-    "https://sctapi.ftqq.com/" +
-    user2.scKey +
-    ".send?title=学号" +
-    user2.userId;
-  if ((user2.status = "success")) {
-    msg = msg + "健康打卡成功";
-    if (user2.screenshot != null) {
-      console.log("发送截图");
-      msg = msg + "&desp=" + user2.screenshotUri;
+const createMessageSummary = async (userStatus) => {
+  var message = [];
+  for (const userId in userStatus) {
+    let status = userStatus[userId];
+    if (status == "success") {
+      message.push("学号" + userId + "健康打卡成功");
+    } else if (status == "wrongpassword") {
+      message.push("学号" + userId + "密码错误");
+    } else if (status == "retryfailed") {
+      message.push("学号" + userId + "多次重试失败");
+    } else if (status == "failed") {
+      message.push("学号" + userId + "健康打卡失败");
+    } else if (status == "havesigned") {
+      message.push("学号" + userId + "今日已打卡");
     }
-  } else {
-    msg = msg + "健康打卡失败";
   }
-  request(encodeURI(msg));
+  return message;
+};
+const sendToWeChat = async ({ msg, scKey = publicConfig.scKey }) => {
+  logger.info("正发送至微信:" + msg);
+  let url =
+    "https://sctapi.ftqq.com/" +
+    scKey +
+    ".send?title=健康打卡状态通告&desp=" +
+    msg;
+  request(encodeURI(url));
 };
 const login = async (user, page) => {
   const { userId, password } = user;
@@ -38,9 +52,9 @@ const login = async (user, page) => {
   const passwordInput = await page.$("#mobilePassword");
   await passwordInput.type(password); //输入密码
   logger.info("密码输入成功");
-  const needCaptcha = await page.evaluate(async() => {
-    return document.getElementById("cpatchaDiv").style.display != "none"
-  })
+  const needCaptcha = await page.evaluate(async () => {
+    return document.getElementById("cpatchaDiv").style.display != "none";
+  });
   if (needCaptcha) {
     const Tesseract = require("tesseract.js");
     const el = await page.$("#captchaImg");
@@ -50,7 +64,7 @@ const login = async (user, page) => {
       logger: (m) => console.log(m),
     }).then(async ({ data: { text } }) => {
       await captchaInput.type(text.replaceAll(" ", ""));
-    });//准确率极低，不建议使用
+    }); //准确率极低，不建议使用
   }
   await Promise.all([
     page.waitForNavigation({
@@ -60,15 +74,22 @@ const login = async (user, page) => {
   ]);
   const url = await page.url();
   if (url.startsWith("https://workflow.ecust.edu.cn/")) {
-    logger.info("登录成功");
     return true;
   } else return false;
 };
+const clean = async (page) => {
+  const client = await page.target().createCDPSession();
+  await client.send("Network.clearBrowserCookies");
+  await client.send("Network.clearBrowserCache");
+};
+
 (async () => {
   const browser = await puppeteer.launch(browserConfig);
+  let userStatus = {};
   for (let i = 0; i < users.length; i++) {
     const user = users[i];
-    logger.info("学号：" + user.userId + "开始打卡。");
+    logger.userId = user.userId;
+    logger.info("开始打卡");
     const page = await browser.newPage();
     await page.emulate(puppeteer.devices["iPhone X"]);
     try {
@@ -82,10 +103,11 @@ const login = async (user, page) => {
         if (await login(user, page)) {
           logger.info("登录成功");
           break;
-        }else{
+        } else {
           failedTimes++;
-          if(failedTimes>=publicConfig.retryTimes){
+          if (failedTimes >= publicConfig.retryTimes) {
             logger.info("登录失败，退出");
+            userStatus[user.userId] = "retryfailed";
             throw new Error("登录失败");
           }
         }
@@ -103,6 +125,7 @@ const login = async (user, page) => {
           });
         } else {
           logger.info("已打卡，下一个用户");
+          userStatus[user.userId] = "havesigned";
           await page.close();
           continue;
         }
@@ -121,25 +144,23 @@ const login = async (user, page) => {
         saveOrUpdate();
       });
       await page.click(".layui-layer-btn0");
-      logger.info("学号：" + user.userId + "已打卡");
-      if (user.needScreenshot) {
-        const tmpDir = require("os").tmpdir();
-        const date = new Date();
-        await page.screenshot({
-          path: path.join(tmpDir, date.getDate() + user.userId + ".png"),
-        });
-        //uploadtoImg
-        user["screenshotUri"] = "123456";
-      }
-      if (user.scKey != null && user.scKey != "") {
-        sendToWeChat(user);
-      }
+      logger.info("已打卡");
+      userStatus[user.userId] = "success";
+      await clean(page);
       await page.close();
     } catch (e) {
       await page.close();
       logger.info(e);
       i--;
       continue;
+    }
+  }
+  logger.userId = null;
+  const sMsg = await createMessageSummary(userStatus);
+  await sendToWeChat({ msg: sMsg.join("\n") });
+  for (const user of users) {
+    if (user.scKey) {
+      await sendToWeChat({ msg: userStatus[user.userId], scKey: user.scKey });
     }
   }
   await browser.close();
--- config-template.js
+++ config-template.js
@@ -10,13 +10,10 @@ users: [
   {
     userId: "YourUserId", //学号
     password: "YourPassword", //密码
-    needScreenshot: false, //是否需要截图
     scKey: "YourSendkey", //发送到微信的key
   },
 ],
   browser: {
-    headless: false, //关闭无头模式
-    args: ["--start-maximized"], //全屏打开浏览器
-    defaultViewport: { width: 1800, height: 1000 }, //设置浏览器页面尺寸
+    headless: true, //关闭无头模式
   },
 };
